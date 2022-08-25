6622215cb104fda7bab861e1ba889944ae06634e
Noam Meraihia <meraihianoam@gmail.com>
2022-08-25 22:27:18 +0200

    Component Array added

--- pge-core/include/PGE/ecs/pgeEcs.h
+++ pge-core/include/PGE/ecs/pgeEcs.h
@@ -4,6 +4,7 @@
 #include <bitset>
 #include <queue>
 #include <array>
+#include <unordered_map>
 
 #include "PGE/utils/pgeLogger.h"
 
@@ -40,6 +41,87 @@ namespace pge
             std::array<EntitySignature, MAX_ENTITIES> mSignatures{};
 
             uint32_t mLivingEntityCount{};
+        };
+
+        class IComponentArray
+        {
+        public:
+            virtual ~IComponentArray() = default;
+            virtual void EntityDestroyed(EntityID entity) = 0;
+        };
+
+        template <typename T>
+        class ComponentArray : public IComponentArray
+        {
+        public:
+            uint16_t InsertData(EntityID entity, T component)
+            {
+                if (mEntityToIndex.find(entity) != mEntityToIndex.end())
+                {
+                    PGE_LOG(PGELLVL_ERROR, "Component added to same entity (id: %d) more than once", entity);
+                    return PGE_ECS_ERRCODE;
+                }
+
+                size_t newIndex = mSize;
+                mEntityToIndex[entity] = newIndex;
+                mIndexToEntity[newIndex] = entity;
+                mComponentArray[newIndex] = component;
+                ++mSize;
+
+                return PGE_ECS_SUCCESSCODE;
+            }
+
+            uint16_t RemoveData(EntityID entity)
+            {
+                if (mEntityToIndex.find(entity) == mEntityToIndex.end())
+                {
+                    PGE_LOG(PGELLVL_ERROR, "Removing non-existent component");
+                    return PGE_ECS_ERRCODE;
+                }
+
+                size_t indexOfRemovedEntity = mEntityToIndex[entity];
+                size_t indexOfLastElement = mSize - 1;
+                mComponentArray[indexOfRemovedEntity] = mComponentArray[indexOfLastElement];
+
+                EntityID entityOfLastElement = mIndexToEntity[indexOfLastElement];
+                mEntityToIndex[entityOfLastElement] = indexOfRemovedEntity;
+                mIndexToEntity[indexOfRemovedEntity] = entityOfLastElement;
+
+                mEntityToIndex.erase(entity);
+                mIndexToEntity.erase(indexOfLastElement);
+
+                --mSize;
+
+                return PGE_ECS_SUCCESSCODE;
+            }
+
+            T& GetData(EntityID entity)
+            {
+                if (mEntityToIndex.find(entity) == mEntityToIndex.end())
+                {
+                    PGE_LOG(PGELLVL_ERROR, "Retrieving non-existent component.");
+                    T* errDat = NULL;
+                    return *errDat;
+                }
+
+                return mComponentArray[mEntityToIndex[entity]];
+            }
+
+            void EntityDestroyed(EntityID entity) override
+            {
+                if (mEntityToIndex.find(entity) != mEntityToIndex.end())
+                {
+                    RemoveData(entity);
+                }
+            }
+
+        private:
+            std::array<T, MAX_ENTITIES> mComponentArray;
+
+            std::unordered_map<EntityID, size_t> mEntityToIndex;
+            std::unordered_map<size_t, EntityID> mIndexToEntity;
+
+            size_t mSize;
         };
     }
 }
